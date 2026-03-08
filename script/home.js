883cde265c5bd5ffeb56378265f6a388cbf2e8d6
krisnokumarghosh <krishnokumarghosh112@gmail.com>
2026-03-08 15:23:17 +0600

    added closed section

--- script/home.js
+++ script/home.js
@@ -56,6 +56,13 @@ const loadOpenData = () => {
     .then((details) => displayOpenData(details.data.filter(el => el.status === "open")))
 }
 
+const loadClosedData = () =>{
+     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((details) => displayClosedData(details.data.filter(el => el.status === "closed")))
+}
+
 
 
 
@@ -135,7 +142,7 @@ const displayAllData = (issues) =>{
 
 
 const displayOpenData = (open) =>{
-    console.log(open)
+    // console.log(open)
     const openIssueCount = document.getElementById('open-issue-count');
     const openissueContainer = document.getElementById('open-issue-container');
     openIssueCount.innerHTML = "";
@@ -199,5 +206,72 @@ const displayOpenData = (open) =>{
        openissueContainer.appendChild(openCardDiv)
     })
 } 
+
+const displayClosedData = (closed) =>{
+    // console.log(closed);
+    const closedIssueCount = document.getElementById('closed-issue-count');
+    const closedIssueContainer = document.getElementById('closed-issue-container');
+    closedIssueCount.innerHTML = "";
+    closedIssueContainer.innerHTML = "";
+
+    closedIssueCount.innerHTML = `
+    
+        <div class="flex items-center gap-2">
+                    <div class="bg-[#ECE4FF] rounded-full p-2">
+                        <img src="./assets/Aperture.png" alt="" class="h-[27px] w-[27px] ">
+                    </div>
+                    <div>
+                        <h2 class="font-semibold text-[20px] text-[#1F2937]">${closed.length} Issues</h2>
+                        <p class="text-[#64748B]">Track and manage your project issues</p>
+                    </div>
+                </div>
+
+                <!-- top right div -->
+                 <div class="flex gap-3">
+                    <div class="flex items-center gap-1">
+                        <img src="./assets/Ellipse 2.png" alt="">
+                        <p class="font-medium text-[#1F2937]">Open</p>
+                    </div>
+                    <div class="flex items-center gap-1">
+                        <img src="./assets/Ellipse 2 (1).png" alt="">
+                        <p class="font-medium text-[#1F2937]">Closed</p>
+                    </div>
+                 </div>
+    
+    `
+
+    closed.forEach((elem) =>{
+        const closedCardDiv = document.createElement('div');
+
+        closedCardDiv.innerHTML = `
+        
+            <div class="bg-white shadow rounded-md h-64 border-t-3 border-[#A855F7]">
+                    <div class="p-4">
+                    <div class="flex justify-between items-center mb-3">
+                       <img src="./assets/Closed- Status .png" alt="">
+                        <p class="px-[25px] py-1.5 ${elem.priority === "high" ? 'bg-red-300 text-[#F8FAFC]' :'bg-[#FFF8DB] text-[#D97706]'} rounded-full font-medium text-[12px] ">${elem.priority} </p>
+                    </div>
+
+                    <div>
+                        <h3 class="font-semibold text-[14px] text-[#1F2937]">${elem.title}</h3>
+                        <p class="text-[12px] text-[#64748B]">${elem.description}</p>
+                        <div class="flex justify-between mt-3">
+                            ${creatElements(elem.labels)}
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="p-4 border-t border-gray-200 space-y-1">
+                    <p class="text-[#64748B] text-[12px]">#${elem.id} by ${elem.author}</p>
+                    <p class="text-[#64748B] text-[12px]">${elem.createdAt}</p>
+                </div>
+                </div>
+        
+        `
+        closedIssueContainer.appendChild(closedCardDiv);
+    })
+    
+}
 
 loadAllData();
